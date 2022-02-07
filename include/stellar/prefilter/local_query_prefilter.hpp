eda0470471d01770c3635ee3b9228cf3a83336f7
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2022-02-07 14:52:49 +0100

    Add stellar::LocalQueryPrefilter
    Test stellar::LocalQueryPrefilter
    
    Add stellar::LocalQueryPrefilter (split header)
    
    WIP: LocalQueryPrefilter supports prefilter agent virtual base class
    
    WIP: LocalQueryPrefilter use prefilter agent virtual base class
    
    WIP: LocalQueryPrefilter create index within
    
    WIP: local_query_prefilter change base class
    
    LocalQueryPrefilter use prefilter_agents
    
    LocalQueryPrefilter use prefilter
    
    stellar::LocalQueryPrefilter use stellar::IndexOptions
    
    LocalQueryPrefilter use stellar::IndexOptions (TEST)
    
    test/api/stellar/prefilter/local_query_prefilter_test.cpp: pass eps-match
    
    test/api/stellar/prefilter/local_query_prefilter_test.cpp: remove epsilon from prefilter
    
    include/stellar/prefilter/local_query_prefilter.hpp: pass eps-match
    
    include/stellar/prefilter/local_query_prefilter.hpp: remove epsilon from prefilter

---
+++ include/stellar/prefilter/local_query_prefilter.hpp
@@ -0,0 +1,136 @@
+
+#pragma once
+
+#include <seqan3/std/span>
+
+#include <stellar/stellar_database_segment.hpp>
+
+#include <stellar/prefilter/detail/valik_local_query_prefilter_index.hpp>
+#include <stellar/prefilter/detail/valik_local_prefilter.hpp>
+// #include <stellar/prefilter/database_agent_distributer.hpp>
+#include <stellar/prefilter/prefilter.hpp>
+#include <stellar/prefilter/prefilter_agent.hpp>
+#include <stellar/prefilter/prefilter_agents.hpp>
+
+namespace stellar
+{
+
+// Prefilter: filters out the queries for each database sequence.
+// In this case it does not filter out any queries and only builds a single query filter
+template <typename TAlphabet>
+struct LocalQueryPrefilter : stellar::prefilter<TAlphabet>
+{
+    using TQueryFilter = stellar::StellarSwiftPattern<TAlphabet>;
+
+    LocalQueryPrefilter() = delete;
+
+    LocalQueryPrefilter(
+        stellar::IndexOptions const options,
+        StringSet<String<TAlphabet> > const & databases,
+        StringSet<String<TAlphabet> > const & queries,
+        stellar::detail::valik_local_query_prefilter_index queryPrefilterIndex
+    ) :
+        _options{options},
+        _databases{databases},
+        _queries{queries},
+        _queryPrefilterIndex{std::move(queryPrefilterIndex)}
+    {}
+
+    using TQuerySegment = seqan::Segment<seqan::String<TAlphabet> const, seqan::InfixSegment>;
+    using TQuerySegments = std::span<TQuerySegment const>;
+
+    using TDatabaseSegment = stellar::StellarDatabaseSegment<TAlphabet>;
+    using TDatabaseSegments = std::span<TDatabaseSegment const>;
+
+    class Agent;
+
+    virtual stellar::prefilter_agents<TAlphabet> agents(size_t const agentCount, stellar::EPSMatchOptions const epsMatchOptions) override
+    {
+        assert(_querySegments.size() == 0u); // calling agents twice is forbidden
+
+        // _databaseSegments = _splitter.split(_databases, minLength);
+        // TODO: use SPLITTER
+        for (String<TAlphabet> const & query: _queries)
+            if (length(query) >= epsMatchOptions.minLength)
+                _querySegments.emplace_back(query, 0u, length(query));
+
+        stellar::prefilter_agents<TAlphabet> agents{};
+        agents.reserve(agentCount);
+
+        // TODO: use DISTRIBUTER
+        TQuerySegments allQuerySegments{_querySegments.begin(), _querySegments.end()};
+        agents.template emplace_back<Agent>(*this, std::move(allQuerySegments), epsMatchOptions.minLength);
+        // for (TDatabaseSegments & segments: DatabaseAgentDistributer::distribute(_databaseSegments, agentCount))
+        //     agents.emplace_back(*this, std::move(segments));
+
+        return agents;
+    }
+
+private:
+    stellar::IndexOptions const _options{};
+    StringSet<String<TAlphabet> > const & _databases{};
+    StringSet<String<TAlphabet> > const & _queries{};
+    std::vector<TQuerySegment> _querySegments{};
+    stellar::detail::valik_local_query_prefilter_index _queryPrefilterIndex{};
+};
+
+template <typename TAlphabet>
+struct LocalQueryPrefilter<TAlphabet>::Agent : prefilter_agent<TAlphabet>
+{
+    friend LocalQueryPrefilter;
+
+    Agent(LocalQueryPrefilter & prefilter, TQuerySegments querySegments, size_t const minLength)
+        : _prefilter_ptr{&prefilter}, _querySegments{std::move(querySegments)}, _minLength{minLength}
+    {}
+
+    using TBase = prefilter_agent<TAlphabet>;
+public:
+
+    using TPrefilterCallback = typename TBase::TPrefilterCallback;
+
+    virtual void prefilter(TPrefilterCallback callback) override
+    {
+        // TODO: we somehow need to calculate this
+        // TODO: not sure if this expression epsilon * minLength is correct
+        size_t const maxError = 1u;
+        assert(_prefilter_ptr != nullptr);
+
+        auto binIDToDatabaseSegment = [&](size_t const binID) -> TDatabaseSegment
+        {
+            size_t const databaseID = binID; // TODO, retrieve translation from index structure
+            auto const & database = _prefilter_ptr->_databases[databaseID];
+            return TDatabaseSegment{database, 0u, length(database)};
+        };
+
+        for (TQuerySegment querySegment : _querySegments)
+        {
+            seqan::String<TAlphabet> const & query = host(querySegment);
+            std::vector<TDatabaseSegment> _databaseSegments{};
+
+            stellar::detail::valik_local_prefilter(
+                query, // TODO: pass querySegment directly
+                _prefilter_ptr->_queryPrefilterIndex,
+                _minLength,
+                maxError,
+                [&](size_t const bin_id) {
+                    _databaseSegments.emplace_back(binIDToDatabaseSegment(bin_id));
+                });
+
+            if (_databaseSegments.size() > 0)
+            {
+                std::span<TQuerySegment> querySegments{&querySegment, 1};
+                stellar::StellarIndex<TAlphabet> index{querySegments, _prefilter_ptr->_options};
+                // TQueryFilter _queryFilter{querySegment};
+                auto pattern = index.createSwiftPattern();
+                callback(_databaseSegments, pattern);
+            }
+        }
+    }
+
+private:
+    LocalQueryPrefilter * _prefilter_ptr{nullptr};
+    TQuerySegments _querySegments{};
+    size_t _minLength{};
+};
+
+} // namespace stellar
---
+++ test/api/stellar/prefilter/local_query_prefilter_test.cpp
@@ -0,0 +1,154 @@
+#include <gtest/gtest.h>
+
+#include "detail/build_lqpindex.hpp"
+
+#include <stellar/stellar_types.hpp>
+#include <stellar/prefilter/local_query_prefilter.hpp>
+
+TEST(LocalQueryPrefilter, LocalQueryPrefilter)
+{
+    using TAlphabet = seqan::Dna5;
+
+    seqan::StringSet<seqan::String<TAlphabet> > databases{};
+    appendValue(databases, "TAGAC" "TAACCGCAGAACACG" "ACTCCTCTAC");
+    appendValue(databases, "TCAC" "GCCAGTTTAATTTTA" "GGGCTCCAAGT");
+    appendValue(databases, "TACGCATATCTGG" "TAACCGCAGAACACG" "AA");
+
+    auto lqpindex = build_lqpindex();
+    for (auto const & database : databases)
+    {
+        size_t const i = &database - &databases[0];
+        lqpindex.insert_sequence(seqan3::bin_index{i}, database);
+    }
+
+    seqan::StringSet<seqan::String<TAlphabet> > queries{};
+    appendValue(queries, "TAGCCG" "TAACCGCAGAACACG" "GACC"); // matches database[0,2]
+    appendValue(queries, "TA" "GCCAGTTTAATTTTA" "TACCAAGA"); // matches database[1]
+    appendValue(queries, "ACAGCTACCGT"); // matches nothing (does not match minLength)
+    appendValue(queries, "TTGAGACGAAT" "AGAACACGACTCCTCTAC" "CTTGTGGCA"); // matches database[0]
+
+    // Note: TQueryFilter are the queries themselve to check whith which queries the actual filter would have been
+    //       constructed.
+    using TPrefilter = stellar::LocalQueryPrefilter<TAlphabet>;
+    using TPrefilterAgent = stellar::prefilter_agent<TAlphabet>;
+
+    stellar::StellarOptions options{};
+    options.minLength = 15u;
+    options.threadCount = 1u;
+    options.epsilon = {0, 1}; // 0.0
+
+    TPrefilter prefilter{options, databases, queries, std::move(lqpindex)};
+
+    using TDatabaseSegment = stellar::StellarDatabaseSegment<TAlphabet>;
+    using TQuerySegment = seqan::Segment<seqan::String<TAlphabet> const, seqan::InfixSegment>;
+
+    std::vector<size_t> seenQueryIDs{};
+
+    for (TPrefilterAgent & prefilterAgent: prefilter.agents(options.threadCount, options))
+    {
+        using TDatabaseSegments = typename TPrefilter::TDatabaseSegments;
+        using TQueryFilter = typename TPrefilter::TQueryFilter;
+
+        prefilterAgent.prefilter([&](TDatabaseSegments databaseSegmentsSpan, TQueryFilter & queryFilter)
+        {
+            auto & sequences = stellar::StellarIndex<TAlphabet>::sequencesFromPattern(queryFilter);
+            EXPECT_EQ(length(sequences), 1u);
+
+            TQuerySegment const & querySegment = sequences[0];
+
+            auto getQueryID = [&](TQuerySegment const & querySegment)
+            {
+                // NOTE: TQuerySegment, host of segment is the underlying query sequences.
+                return &host(querySegment) - &queries[0];
+            };
+            auto getDatabaseID = [&](TDatabaseSegment const & databaseSegment)
+            {
+                return &databaseSegment.underlyingDatabase() - &databases[0];
+            };
+            auto getDatabaseIDs = [&](TDatabaseSegments const & databaseSegments)
+            {
+                std::vector<size_t> databaseIDs{};
+
+                for (auto && databaseSegment : databaseSegments)
+                    databaseIDs.push_back(getDatabaseID(databaseSegment));
+
+                return databaseIDs;
+            };
+
+            std::vector<TDatabaseSegment> databaseSegments(databaseSegmentsSpan.begin(), databaseSegmentsSpan.end());
+            std::sort(databaseSegments.begin(), databaseSegments.end());
+
+            size_t const queryID = getQueryID(querySegment);
+
+            EXPECT_LT(queryID, seqan::length(queries));
+
+            seenQueryIDs.push_back(queryID);
+
+            switch (queryID)
+            {
+                case 0:
+                    EXPECT_EQ(querySegment, TQuerySegment(queries[0], 0, seqan::length(queries[0])));
+
+                    EXPECT_EQ(databaseSegments.size(), 2);
+                    EXPECT_EQ(getDatabaseIDs(databaseSegments), (std::vector<size_t>{0, 2}));
+                    EXPECT_EQ(databaseSegments,
+                        (std::vector<TDatabaseSegment>{
+                            // D0: TAGAC [TAACCGCAGAACACG]ACTCCTCTAC
+                            //            |||||||||||||||
+                            // Q0: TAGCCG[TAACCGCAGAACACG]GACC"
+                            {databases[0], 0, seqan::length(databases[0])},
+                            // D2: TACGCATATCTG[GTAACCGCAGAACACG]AA
+                            //                  ||||||||||||||||
+                            // Q0: TAGCC       [GTAACCGCAGAACACG]GACC
+                            {databases[2], 0, seqan::length(databases[2])}
+                        })
+                    );
+                break;
+                case 1:
+                    EXPECT_EQ(querySegment, TQuerySegment(queries[1], 0, seqan::length(queries[1])));
+
+                    EXPECT_EQ(databaseSegments.size(), 1);
+                    EXPECT_EQ(getDatabaseIDs(databaseSegments), (std::vector<size_t>{1}));
+                    EXPECT_EQ(databaseSegments,
+                        (std::vector<TDatabaseSegment>{
+                            // D1: TCAC[GCCAGTTTAATTTTA]GGGCTCCAAGT
+                            //          |||||||||||||||
+                            // Q1: TA  [GCCAGTTTAATTTTA]TACCAAGA
+                            {databases[1], 0, seqan::length(databases[1])}
+                        })
+                    );
+                break;
+                // case 2: will been filtered out due to being to short
+                case 3:
+                    EXPECT_EQ(querySegment, TQuerySegment(queries[3], 0, seqan::length(queries[3])));
+
+                    EXPECT_EQ(databaseSegments.size(), 2);
+                    EXPECT_EQ(getDatabaseIDs(databaseSegments), (std::vector<size_t>{0, 2}));
+                    EXPECT_EQ(databaseSegments,
+                        (std::vector<TDatabaseSegment>{
+                            // D0: TAGACTAACCGC[AGAACACGACTCCTCTAC]
+                            //                  ||||||||||||||||||
+                            // Q3: TTGAGACGAAT [AGAACACGACTCCTCTAC]CTTGTGGCA
+                            {databases[0], 0, seqan::length(databases[0])},
+                            // NOTE: This is a false-positive
+                            // D2: TACGCATATCTGGTAACCGC[AGAACACGA]A
+                            //                          |||||||||
+                            // Q3: TTGAGACGAAT         [AGAACACGA]CTCCTCTACCTTGTGGCA
+                            {databases[2], 0, seqan::length(databases[2])},
+                        })
+                    );
+                break;
+                default:
+                    ADD_FAILURE() << "There should be only 4 queryIDs";
+                break;
+            };
+        });
+    }
+
+    // Note: every query should have been seen completely
+    std::sort(seenQueryIDs.begin(), seenQueryIDs.end());
+    EXPECT_EQ(seenQueryIDs, (std::vector<size_t>
+    {
+        0, 1/*, 2 (will been filtered out due to beeing to short)*/, 3
+    }));
+}
