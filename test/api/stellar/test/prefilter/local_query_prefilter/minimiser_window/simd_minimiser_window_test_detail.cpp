59192d720d39d51902fb5426f3bfe5957bec048c
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2022-08-02 17:25:45 +0200

    FIX backward

--- test/api/stellar/test/prefilter/local_query_prefilter/minimiser_window/simd_minimiser_window_test_detail.cpp
+++ test/api/stellar/test/prefilter/local_query_prefilter/minimiser_window/simd_minimiser_window_test_detail.cpp
@@ -219,7 +219,7 @@ void compute_backward_chunk(size_t const window_size, int const * __restrict sou
 {
     int current_minimiser = std::numeric_limits<int>::max();
     size_t current_offset = 0;
-    for (size_t offset = 0; offset < window_size - 1; ++offset, ++source_ptr, ++target_ptr, ++offset_ptr)
+    for (size_t offset = 0; offset < window_size; ++offset, ++source_ptr, ++target_ptr, ++offset_ptr)
     {
         bool smaller = *source_ptr < current_minimiser;
         current_minimiser = smaller ? *source_ptr : current_minimiser;
@@ -259,14 +259,26 @@ void compute_forward_full(size_t const window_size, int const * __restrict sourc
 
 void compute_backward_full(size_t const window_size, int const * __restrict source_ptr, int * __restrict target_ptr, size_t const target_size, int * __restrict offset_ptr)
 {
-    int * const target_end_ptr = target_ptr + target_size;
-    for (; target_ptr < target_end_ptr ;)
+    std::ptrdiff_t remaining_elements = target_size % (window_size - 1);
+    int * const target_peeled_end_ptr = target_ptr + target_size - remaining_elements;
+    for (; target_ptr < target_peeled_end_ptr ;)
     {
-        compute_backward_chunk(window_size, source_ptr, target_ptr, offset_ptr);
+        compute_backward_chunk(window_size - 1, source_ptr, target_ptr, offset_ptr);
 
-        source_ptr += window_size;
+        source_ptr += window_size - 1;
         target_ptr += window_size - 1;
         offset_ptr += window_size - 1;
+    }
+
+    if (remaining_elements > 0)
+    {
+        // source_ptr -= window_size - 1;
+        // target_ptr -= window_size - 1;
+        // offset_ptr -= window_size - 1;
+        // source_ptr += remaining_elements;
+        // target_ptr += remaining_elements;
+        // offset_ptr += remaining_elements;
+        compute_backward_chunk(remaining_elements, source_ptr, target_ptr, offset_ptr);
     }
 }
 
@@ -547,24 +559,28 @@ int main()
     };
 
     std::vector<int> window_value_backward_expected{
-        3 ,3 /*8 */,3 /*5 */,3 /*4 */,3 /*10*/,//3 /*17*/,
-        9 ,9 /*15*/,9 /*20*/,9 /*13*/,9 /*19*/,//9 /*13*/,
-        0 ,0 /*1 */,0 /*4 */,0 /*0 */,0 /*8 */,//0 /*18*/,
-        8 ,8 /*16*/,8 /*14*/,8 /*9 */,8 /*14*/,//8 /*8 */,
-        19,3 /*3 */,3 /*3 */,3 /*9 */,3 /*5 */,//3 /*20*/,
-        0 ,0 /*1 */,0 /*2 */,0 /*3 */,0 /*4 */,//0 /*5 */,
-        6 ,6 /*7 */,6 /*8 */,6 /*9 */,6 /*10*/,//6 /*11*/,
-        11,10/*10*/,9 /*9 */,8 /*8 */,7 /*7 */,//6 /*6 */,
+        /*1:*/ 3 /*3 */,3 /*8 */,3 /*5 */,3 /*4 */,3 /*10*/,
+        /*2:*/ 17/*17*/,9 /*9 */,9 /*15*/,9 /*20*/,9 /*13*/,
+        /*3:*/ 19/*19*/,13/*13*/,0 /*0 */,0 /*1 */,0 /*4 */,
+        /*4:*/ 0 /*0 */,0 /*8 */,0 /*18*/,0 /*8 */,0 /*16*/,
+        /*5:*/ 14/*14*/,9 /*9 */,9 /*14*/,8 /*8 */,8 /*19*/,
+        /*6:*/ 3 /*3 */,3 /*3 */,3 /*9 */,3 /*5 */,3 /*20*/,
+        /*7:*/ 0 /*0 */,0 /*1 */,0 /*2 */,0 /*3 */,0 /*4 */,
+        /*8:*/ 5 /*5 */,5 /*6 */,5 /*7 */,5 /*8 */,5 /*9 */,
+        /*9:*/ 10/*10*/,10/*11*/,10/*11*/,10/*10*/,9 /*9 */,
+        /*0:*/ 8 /*8 */,7 /*7 */,6 /*6 */,
     };
     std::vector<int> window_offset_backward_expected{
-        0, 0, 0, 0, 0, //0,
-        0, 0, 0, 0, 0, //0,
-        0, 0, 0, 0, 0, //0,
-        0, 0, 0, 0, 0, //0,
-        0, 1, 1, 1, 1, //1,
-        0, 0, 0, 0, 0, //0,
-        0, 0, 0, 0, 0, //0,
-        0, 1, 2, 3, 4, //5,
+        /*1:*/ 0, 0, 0, 0, 0,
+        /*2:*/ 0, 1, 1, 1, 1,
+        /*3:*/ 0, 1, 2, 2, 2,
+        /*4:*/ 0, 0, 0, 0, 0,
+        /*5:*/ 0, 1, 1, 3, 3,
+        /*6:*/ 0, 0, 0, 0, 0,
+        /*7:*/ 0, 0, 0, 0, 0,
+        /*8:*/ 0, 0, 0, 0, 0,
+        /*9:*/ 0, 0, 0, 0, 4,
+        /*0:*/ 0, 1, 2,
         // 5, 5, 5, 5, 5, 5,
     };
 
@@ -583,7 +599,7 @@ int main()
     window_offset_backward.resize(values.size());
 
     compute_forward_full(window_size, values.data(), window_value_forward.data(), window_value_forward.size(), window_offset_forward.data());
-    compute_backward_simd(window_size, values.data(), window_value_backward.data(), window_value_backward.size(), window_offset_backward.data());
+    compute_backward_full(window_size, values.data(), window_value_backward.data(), window_value_backward.size(), window_offset_backward.data());
 
     std::cout << "window_value_forward: " << std::endl;
     print_chunked(window_value_forward, window_size - 1);
